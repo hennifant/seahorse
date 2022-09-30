a53be8282b1767206c628ea6111a3a4ce8fcc7a9
hennifant <henning.schwichow@gmx.de>
2022-09-30 15:34:32 +0200

    win and lose condition

--- script.js
+++ script.js
@@ -144,6 +144,31 @@ window.addEventListener("load", function () {
       for (let i = 0; i < this.game.ammo; i++) {
         context.fillRect(20 + 5 * i, 50, 3, 20);
       }
+      // messages
+      if (this.game.gameOver) {
+        context.textAlign = "center";
+        let message1;
+        let message2;
+        if (this.game.score > this.game.winningScore) {
+          message1 = "You Win!";
+          message2 = "Awesome!";
+        } else {
+          message1 = "You lose!";
+          message2 = "Try harder!";
+        }
+        context.font = "50px " + this.fontFamily;
+        context.fillText(
+          message1,
+          this.game.width * 0.5,
+          this.game.height * 0.5 - 40
+        );
+        context.font = "25px " + this.fontFamily;
+        context.fillText(
+          message2,
+          this.game.width * 0.5,
+          this.game.height * 0.5 + 40
+        );
+      }
       context.restore();
     }
   }
