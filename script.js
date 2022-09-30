5ec05eaf088d6e2a8d04d463ad80223be1947b66
hennifant <henning.schwichow@gmx.de>
2022-09-30 19:16:47 +0200

    Background layer

--- script.js
+++ script.js
@@ -119,9 +119,39 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Layer {}
+  class Layer {
+    constructor(game, image, speedModifier) {
+      this.game = game;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.width = 1768;
+      this.height = 500;
+      this.x = 0;
+      this.y = 0;
+    }
+    update() {
+      if (this.x <= -this.width) this.x = 0;
+      else this.x -= this.game.speed * this.speedModifier;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y);
+    }
+  }
 
-  class Background {}
+  class Background {
+    constructor(game) {
+      this.game = game;
+      this.image1 = document.getElementById("layer1");
+      this.layer1 = new Layer(this.game, this.image1, 1);
+      this.layers = [this.layer1];
+    }
+    update() {
+      this.layer1.forEach((layer) => layer.update());
+    }
+    draw(context) {
+      this.layers.forEach((layer) => layer.draw(context));
+    }
+  }
 
   class UI {
     constructor(game) {
@@ -179,6 +209,7 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
       this.ui = new UI(this);
@@ -195,10 +226,12 @@ window.addEventListener("load", function () {
       this.winningScore = 10;
       this.gameTime = 0;
       this.timeLimit = 5000;
+      this.speed = 1;
     }
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
       if (this.gameTime > this.timeLimit) this.gameOver = true;
+      this.background.update();
       this.player.update();
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
@@ -232,6 +265,7 @@ window.addEventListener("load", function () {
       }
     }
     draw(context) {
+      this.background.draw(context);
       this.player.draw(context);
       this.ui.draw(context);
       this.enemies.forEach((enemy) => {
