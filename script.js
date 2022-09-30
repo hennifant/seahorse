e79af87793f17106bb5824af18c63b6d45fb4b33
hennifant <henning.schwichow@gmx.de>
2022-09-30 12:12:13 +0200

    Ammo UI

--- script.js
+++ script.js
@@ -95,7 +95,21 @@ window.addEventListener("load", function () {
 
   class Background {}
 
-  class UI {}
+  class UI {
+    constructor(game) {
+      this.game = game;
+      this.fontSize = 25;
+      this.fontFamily = "Helvetica";
+      this.color = "white";
+    }
+    draw(context) {
+      // ammo
+      context.fillStyle = this.color;
+      for (let i = 0; i < this.game.ammo; i++) {
+        context.fillRect(20 + 5 * i, 50, 3, 20);
+      }
+    }
+  }
 
   class Game {
     constructor(width, height) {
@@ -103,21 +117,25 @@ window.addEventListener("load", function () {
       this.height = height;
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.ui = new UI(this);
       this.keys = [];
       this.ammo = 20;
       this.maxAmmo = 50;
       this.ammoTimer = 0;
       this.ammoInterval = 500;
     }
-    update() {
+    update(deltaTime) {
       this.player.update();
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
         this.ammoTimer = 0;
+      } else {
+        this.ammoTimer += deltaTime;
       }
     }
     draw(context) {
       this.player.draw(context);
+      this.ui.draw(context);
     }
   }
 
