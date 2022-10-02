103e119beca098e1e194dc658861fe85d2bcc02a
hennifant <henning.schwichow@gmx.de>
2022-10-02 18:49:08 +0200

    player assets fix

--- script.js
+++ script.js
@@ -55,6 +55,9 @@ window.addEventListener("load", function () {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 1;
+      this.maxFrame = 37;
       this.speedY = 0;
       this.maxSpeed = 3;
       this.projectiles = [];
@@ -66,17 +69,34 @@ window.addEventListener("load", function () {
         this.speedY = this.maxSpeed;
       else this.speedY = 0;
       this.y += this.speedY;
-      // hande projectiles
+      // handle projectiles
       this.projectiles.forEach((projectiles) => {
         projectiles.update();
       });
       this.projectiles = this.projectiles.filter(
         (projectile) => !projectile.markedForDeletion
       );
+      // sprite animation
+      if (this.frameX < this.maxFrame) {
+        this.frameX++;
+      } else {
+        this.frameX = 0;
+      }
     }
     draw(context) {
       context.fillStyle = "black";
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
       this.projectiles.forEach((projectiles) => {
         projectiles.draw(context);
       });
