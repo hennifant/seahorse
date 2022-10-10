eb752a9eeeda70430fd08fbe5f4ad6c560cacd74
hennifant <henning.schwichow@gmx.de>
2022-10-10 17:09:07 +0200

    Refactoring and tweaks

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ window.addEventListener("load", function () {
       this.angle = 0;
       this.va = Math.random() * 0.2 - 0.1;
       this.bounced = 0;
-      this.bottomBounceBoundary = Math.random() * 100 + 60;
+      this.bottomBounceBoundary = Math.random() * 80 + 60;
     }
     update() {
       this.angle += this.va;
@@ -194,7 +194,8 @@ window.addEventListener("load", function () {
     enterPowerUp() {
       this.powerUpTimer = 0;
       this.powerUp = true;
-      this.game.ammo = this.game.maxAmmo;
+      if (this.game.ammo < this.game.maxAmmo)
+        this.game.ammo = this.game.maxAmmo;
     }
   }
   class Enemy {
@@ -393,7 +394,7 @@ window.addEventListener("load", function () {
       this.gameTime = 0;
       this.timeLimit = 15000;
       this.speed = 1;
-      this.debug = true;
+      this.debug = false;
     }
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
@@ -424,7 +425,7 @@ window.addEventListener("load", function () {
               )
             );
           }
-          if ((enemy.type = "lucky")) this.player.enterPowerUp();
+          if (enemy.type === "lucky") this.player.enterPowerUp();
           else this.score--;
         }
         this.player.projectiles.forEach((projectile) => {
@@ -465,8 +466,9 @@ window.addEventListener("load", function () {
     }
     draw(context) {
       this.background.draw(context);
-      this.player.draw(context);
       this.ui.draw(context);
+      this.player.draw(context);
+
       this.particles.forEach((particle) => particle.draw(context));
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
@@ -478,7 +480,6 @@ window.addEventListener("load", function () {
       if (randomize < 0.3) this.enemies.push(new Angler1(this));
       else if (randomize < 0.6) this.enemies.push(new Angler2(this));
       else this.enemies.push(new LuckyFish(this));
-      console.log(this.enemies);
     }
     checkCollision(rect1, rect2) {
       return (
